cb1c190adfc6ae4b9f98889bf49c6178cc1fe62b
Harbansi Darji <darji.harbansi@gmail.com>
2023-03-18 23:43:54 +0530

    made some changes

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-const index = 12;
+const express = require("express");
+const bodyParser = require("body-parser");
 
-console.log(index);
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var calc = num1 + num2;
+
+  res.send("the calculate ans is: " + calc);
+});
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/BMIcalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  var height = parseFloat(req.body.height);
+  var weight = parseFloat(req.body.weight);
+
+  var result = weight / (height * height);
+
+  res.send("your BMI is: " + result);
+});
+
+app.listen("3000", function () {
+  console.log("the file is serving on the local host 3000");
+});
+console.log("abcdfindex");
